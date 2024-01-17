53716dc90be386144d2be87e44daa37301c5b76a
dhckdwls <codetama51@gmail.com>
2024-01-17 14:38:21 +0900

    24년1월17일 재시험

---
+++ src/WiseSaying.java
@@ -0,0 +1,47 @@
+
+public class WiseSaying {
+	private int id;
+	private String wise;
+	private String author;
+	
+	public WiseSaying(int id,String wise,String author) {
+		this.id = id;
+		this.wise = wise;
+		this.author = author;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getWise() {
+		return wise;
+	}
+
+
+	public void setWise(String wise) {
+		this.wise = wise;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+	
+	
+	
+}
